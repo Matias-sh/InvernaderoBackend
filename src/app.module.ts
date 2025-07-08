3cae677dc2e49d6eb483ef83cc6430d00a769d39
Matias-sh <devmatiasbritez@gmail.com>
2025-07-08 04:34:09 -0300

    feat: agregar ruta /status para healthcheck

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller'; // 👈 Importamos el nuevo controller
 import { GreenhouseModule } from './greenhouse/greenhouse.module';
 
 @Module({
   imports: [GreenhouseModule],
+  controllers: [AppController], // 👈 Lo registramos
 })
 export class AppModule {}
